edd72cf2e47ff3051967fca5e06a86c5c1870508
Amin Kamal <aminkamal355@gmail.com>
2022-12-18 21:44:19 +0800

    tugas 22 js
    tugas 22 js

---
+++ tugas22.js
@@ -0,0 +1,9 @@
+function Panggil(){
+    var data = ['Saya', 'ingin', 'belajar', 'bersama']
+    data.forEach(function(AminKamal,index){
+        console.log("item :", AminKamal, "adalah index ke",index)
+    })
+    
+}
+
+Panggil()
